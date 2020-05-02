637eaf1bbcd87a75b578625a0db5ffe53e150ef7
Guilherme Keiel <gkeiel@hotmail.com>
2020-05-02 01:50:57 -0300

    v1.0.0

---
+++ control_systems/SISO_state_feedback/README.md
@@ -0,0 +1,3 @@
+# SISO_state_feedback
+
+A code to compute state-feedback gains for a controllable SISO system. Although matching the characteristic equation or controllable canonical transfomation methods, I consider Moore's algorithm one practical way.
---
+++ control_systems/SISO_state_feedback/SISO_state_feedback.m
@@ -0,0 +1,19 @@
+clc; clear;
+
+% declare SISO system
+A = [0  1  0  0;
+     0  0 -1  0;
+     0  0  0  1;
+     0  0  5  0]; 
+B = [0; 1; 0; 2];
+C = [1  0  0  0];
+
+lamb_A = eig(A);            % eigenvalues of A
+lamb_T = [-10 -20 -30 -40]; % desired closed-loop eigenvalues
+
+% Moore method
+[ K, lamb_E ] = moore_method( A, B, lamb_T );
+
+% print result
+fprintf('K           = [ %s] \n', sprintf('%.2f ', K));
+fprintf('eigenvalues = [ %s] \n', sprintf('%.2f ', lamb_T));
