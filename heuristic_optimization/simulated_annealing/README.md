6e567b611ec966db35116fa43f9c5583b9f09728
Guilherme Keiel <gkeiel@hotmail.com>
2020-05-09 03:11:02 -0300

    v1.0.0

---
+++ heuristic_optimization/simulated_annealing/README.md
@@ -0,0 +1,3 @@
+# simulated_annealing
+
+A simple simulated_annealing algorithm for finding the minimum of a function. Objective function must be provided. Although a fixed temperature decay rate is used, its method can be easily modified.
--- mathematical_optimization/gradient_descent/gradient_descent.m
+++ mathematical_optimization/gradient_descent/gradient_descent.m
@@ -1,27 +1,32 @@
-% gradient descent
-clc; clear all;
-k = 1; k_max = 20; f = []; g = []; % initializing variables 
+% gradient descent algorithm
+clc; clear all; k = 1; k_max = 30;
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%% entry of parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%
-ep = 1e-3;    % tolerance
-x  = [1; 10]; % initial guess
-alpha = 2e-1; % step size
-F  = @(x1,x2) x1.^2 +2.*x2.^2 +x1.*x2 -6.*x1 -10.*x2; % objective function
-Gr = @(x1,x2) [2*x1+x2-6; 4*x2+x1-10];                % analytical gradient
-show = @(k,x) fprintf('k = %2d: x = %-10s, F(x) = %.4f\n', k, mat2str(x,2), F(x(1),x(2)));
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% tolerance, initial solution, size step
+ep    = 1e-3;
+x     = [1; 10];
+alpha = 2e-1;
 
-f(k)   = F(x(1),x(2));      % calculate objective for x_0
-g(:,k) = Gr(x(1),x(2));     % calculate gradient for x_0
-show(k-1,x);                % show initial values
-while ( k < k_max && abs(g(end)) > ep)
+% objective function
+F  = @(x1,x2) x1.^2 +2.*x2.^2 +x1.*x2 -6.*x1 -10.*x2;
+
+% analytical gradient
+Gr = @(x1,x2) [2*x1+x2-6; 4*x2+x1-10];
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% gradient descent %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+f(k) = F(x(1),x(2));        % evaluate objective for x_0
+fprintf('k = %2d: x = %-10s| F(x) = %.4f\n', k, mat2str(x,2), f(k));
+
+while( k<k_max && abs( max(Gr(x(1),x(2))) )>ep ) % maximum iterations or optimality stop criterion
     k = k+1;
     d = -Gr(x(1),x(2));     % descent direction
     x = x +alpha*d;         % gradient descent 
 
     f(k) = F(x(1),x(2));    % evaluate objective function
-    show(k-1,x);            % show progress
+    fprintf('k = %2d: x = %-10s| F(x) = %.4f\n', k, mat2str(x,2), f(k));    % show progress
 end
 
 plot(0:k-1, f, 'LineWidth',2.5, 'color','k'); grid on;
-title('Objective Function'); xlabel('Iteration'); ylabel('F(x)');
+title('Objective function evolution'); xlabel('Iteration'); ylabel('F(x)');
