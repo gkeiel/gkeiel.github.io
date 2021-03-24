5a930e0b2513955133f3ee799044f0fcef368848
Guilherme Keiel <gkeiel@hotmail.com>
2021-03-24 19:49:40 -0300

    v1.0.0

--- system_identification/kalman_filter/kalman_filter.m
+++ system_identification/kalman_filter/kalman_filter.m
@@ -1,4 +1,4 @@
-function [ x, y, K ] = kalman_filter( sysd, u, w, z, t, x_0, P_0, R, Q )
+function [ x, y, K_m, P_m ] = kalman_filter( sysd, u, w, z, t, x_0, P_0, R, Q )
 
 % discrete-time model
 F = sysd.a;
@@ -11,6 +11,7 @@ k = 1 +1;
 x = x_0;                 
 P = P_0;
 I = diag( ones(length(F),1) );
+K_m = []; P_m = [];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% kalman filter %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 for n = 1:length(t)-1
@@ -20,13 +21,16 @@ for n = 1:length(t)-1
     P      = F*P*F' +J*Q*J';                    % 'a priori' predicted covariance
 
     % 2. correction
-    K      = P*H'*inv( H*P*H' +R );             % compute optimal gain   
+    K      = P*H'*inv( H*P*H' +R );             % computes optimal gain   
     e(k)   = z(k) -H*x(:,k);                    % measurement residual
     x(:,k) = x(:,k) +K*e(k);                    % 'a posteriori' state estimative 
     P      = (I -K*H)*P;                        % 'a posteriori' covariance
     
     y(k)   = H*x(:,k);                          % estimated output
-    k      = k +1;
+    k      = k +1;                              
+    
+    K_m    = [K_m K];                           % stores current K
+    P_m    = [P_m P];                           % stores current P            
 end
 
 x = transpose(x);
--- system_identification/kalman_filter/kalman_filter_main.m
+++ system_identification/kalman_filter/kalman_filter_main.m
@@ -2,10 +2,10 @@ clc; clear; close all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SYSTEM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % import system
-% x'(t) = Ax(t) +Bu(t) +Iw(t)
+% x'(t) = Ax(t) +Bu(t) +Ew(t)
 %  y(t) = Cx(t) +v(t)
 [ sys ] = sys_RC;
-% [ sys ] = sys_RLC;
+%[ sys ] = sys_RLC;
 
 % discrete-time model equivalent
 % x(k+1) = Fx(k) +Gu(k) +Jw(k)
@@ -14,27 +14,27 @@ T        = 1/1000;
 sysd     = c2d( sys,T,'zoh' );
 
 % define input and disturbance signals
-Tf    = 0.10;
+Tf    = 0.10;                               % simulation time                
 [u,t] = gensig('square',Tf/4,Tf,T);         % input and time
-q     = 0^2;                              % variance of process
-r     = 0.5^2;                              % variance of measurement
-w     = q*randn(length(t),length(sys.A));   % gaussian noise with covariance Q
+q     = 0.1;                                % variance of process
+r     = 0.4;                                % variance of measurement
+w     = q*randn(length(t),1);               % gaussian noise with covariance Q
 v     = r*randn(length(t),1);               % gaussian noise with variance R
 
-% simulate system WITH process and measurement NOISES
+% simulate system WITH process and measurement NOISE
 [z,t,x_] = lsim( sysd, [u w], t );
 z        = z +v;
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% KALMAN FILTER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-x_0 = 0.5*ones( length(sys.A),1 );          % initial condition
-P_0 = 100*diag( ones( length(sys.A),1 ) );    % initial covariance
-[ x, y, K ] = kalman_filter( sysd, u, w', z, t, x_0, P_0, r, q );
+x_0 = 0.1*ones( length(sys.A),1 );          % initial condition
+P_0 = 10*diag( ones( length(sys.A),1 ) );   % initial covariance
+[ x, y, K_m, P_m ] = kalman_filter( sysd, u, w', z, t, x_0, P_0, r, q );
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% RESULTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%fprintf('K = [ %s] \n', sprintf('%.4f ', K));
+[y_o,t_o,x_o] = lsim( sys, [u 0*w], t);     % ideal system for comparison
 
-[y_o,t_o,x_o] = lsim( sysd, [u 0*w], t);    % real system for comparison
 figure(1)
 for k = 1:length(sys.A)
     subplot( round((length(sys.A)+1)/2 ),2,k)
